0c1c687d41f24c13f5515c56faba14b265f4ac43
Grisha GHUKASYAN <grisha@brigad.co>
2021-01-19 18:17:40 +0100

    fix: code

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@
 import { args } from './args';
 import { decryptAll } from './decrypt';
 import { diffFile, diffList } from './diff';
+import { encryptAll } from './encrypt';
 
 const actions = {
   decrypt: decryptAll,
-  encrypt: decryptAll,
+  encrypt: encryptAll,
   'diff-list': diffList,
   'diff-file': diffFile,
 };
